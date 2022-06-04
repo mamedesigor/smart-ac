becebbf3907c3a388c81311a7c007efc1b476a8b
mamedesigor <mamedesigor@gmail.com>
2022-06-04 16:16:09 -0400

    get mqqt related constants from config file

--- smart_ac.py
+++ smart_ac.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+MQQT_TOPICS = []
 with open("config.h", "r") as config:
     for line in config:
         splitted_line = line.split();
@@ -8,3 +8,13 @@ with open("config.h", "r") as config:
                 SSID = splitted_line[i+1].replace('"', '')
             elif word == "PASSWORD":
                 PASSWORD = splitted_line[i+1].replace('"', '')
+            elif word == "MQQT_BROKER":
+                MQQT_BROKER = splitted_line[i+1].replace('"', '')
+            elif word == "MQQT_PORT":
+                MQQT_PORT = splitted_line[i+1].replace('"', '')
+            elif word == "TEMP1_TPC":
+                TEMP1_TPC = splitted_line[i+1].replace('"', '')
+                MQQT_TOPICS.append(TEMP1_TPC)
+            elif word == "TEMP2_TPC":
+                TEMP2_TPC = splitted_line[i+1].replace('"', '')
+                MQQT_TOPICS.append(TEMP2_TPC)
