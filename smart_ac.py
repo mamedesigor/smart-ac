d2cd6088b40e9bba51c20b734a307490e5899100
mamedesigor <mamedesigor@gmail.com>
2022-06-04 15:56:57 -0400

    get ssid and password from config file

--- smart_ac.py
+++ smart_ac.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+with open("config.h", "r") as config:
+    for line in config:
+        splitted_line = line.split();
+        for i, word in enumerate(splitted_line):
+            if word == "SSID":
+                SSID = splitted_line[i+1].replace('"', '')
+            elif word == "PASSWORD":
+                PASSWORD = splitted_line[i+1].replace('"', '')
