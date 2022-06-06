b02e28a96123c8405bda9456bfc48aac47ad3e71
Igor Mamedes <mamedesigor@gmail.com>
2022-06-06 14:44:40 -0400

    Add files via upload

---
+++ docs/mqtt.js
@@ -0,0 +1,33 @@
+// Create a client instance
+client = new Paho.MQTT.Client("broker.hivemq.com", Number(8000), "clientId");
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({onSuccess:onConnect});
+
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("World");
+  message = new Paho.MQTT.Message("Hello");
+  message.destinationName = "World";
+  client.send(message);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    document.getElementById("demo").innerHTML = message.payloadString;
+    console.log("onMessageArrived:"+message.payloadString);
+}
