f2b40f0fb53c7555aa44f7df81a10ad9c9e4fa8a
mamedesigor <mamedesigor@gmail.com>
2022-06-12 21:40:18 -0400

    add all mqtt topics

--- smart_ac.py
+++ smart_ac.py
@@ -16,15 +16,39 @@ with open("config.h", "r") as config:
                 MQTT_BROKER = splitted_line[i+1].replace('"', '')
             elif word == "MQTT_PORT":
                 MQTT_PORT = int(splitted_line[i+1].replace('"', ''))
-            elif word == "TEMP1_TPC":
-                TEMP1_TPC = splitted_line[i+1].replace('"', '')
-                MQTT_TOPICS.append((TEMP1_TPC, 0))
-            elif word == "TEMP2_TPC":
-                TEMP2_TPC = splitted_line[i+1].replace('"', '')
-                MQTT_TOPICS.append((TEMP2_TPC, 0))
+            elif word == "BMP280":
+                BMP280 = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((BMP280, 0))
+            elif word == "HTU21D":
+                HTU21D = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((HTU21D, 0))
             elif word == "CCS811":
                 CCS811 = splitted_line[i+1].replace('"', '')
                 MQTT_TOPICS.append((CCS811, 0))
+            elif word == "SDS011":
+                SDS011 = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((SDS011, 0))
+            elif word == "BH1750":
+                BH1750 = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((BH1750, 0))
+            elif word == "MICS6814":
+                MICS6814 = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((MICS6814, 0))
+            elif word == "SCT013_1":
+                SCT013_1 = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((SCT013_1, 0))
+            elif word == "SCT013_2":
+                SCT013_2 = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((SCT013_2, 0))
+            elif word == "REEDSWITCH":
+                REEDSWITCH = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((REEDSWITCH, 0))
+            elif word == "PIR":
+                PIR = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((PIR, 0))
+            elif word == "IR":
+                IR = splitted_line[i+1].replace('"', '')
+                MQTT_TOPICS.append((IR, 0))
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
