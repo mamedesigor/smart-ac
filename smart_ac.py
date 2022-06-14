bec82b6dbf2f1e4b22e363d0870ca6855beb883e
mamedesigor <mamedesigor@gmail.com>
2022-06-13 21:07:37 -0400

    get mqtt msg and store in a buffer with json format

--- smart_ac.py
+++ smart_ac.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
 import paho.mqtt.client as mqtt
+import time
+import json
 import config
+
+buffer = config.buffer
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe(config.MQTT_TOPICS)
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    buffer.get(msg.topic).append(json.loads(msg.payload.decode("utf-8")))
 
 client = mqtt.Client()
 client.on_connect = on_connect
