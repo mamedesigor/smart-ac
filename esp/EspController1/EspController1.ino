165dd854f016bf7f2778ec4adeda62c9865aa1f9
mamedesigor <mamedesigor@gmail.com>
2022-07-10 22:09:10 -0400

    refactor i2c and ccs811 code for better reading

--- esp/EspController1/EspController1.ino
+++ esp/EspController1/EspController1.ino
@@ -10,32 +10,14 @@ long timer = 0;
 CCS811 ccs811(-1);
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
-char mqttMsg[40];
+char ccs811Reading[40];
 
 void setup() {
 	Serial.begin(115200);
 	setupWifi();
 	setupMqtt();
-
-	//setup I2C
-	Serial.print("Setup: I2C ");
-	Wire.begin();
-	Serial.println("OK");
-
-	//enable CCS811
-	bool ok;
-	Serial.print("Setup: CCS811 ");
-	ccs811.set_i2cdelay(50);
-	ok = ccs811.begin();
-	if (ok) Serial.println("OK"); else Serial.println("FAILED");
-
-	//start CCS811
-	Serial.print("Setup: CCS811 start ");
-	ok = ccs811.start(CCS811_MODE_1SEC);
-	if (ok) Serial.println("OK"); else Serial.println("FAILED");
-
-	//CCS811 version
-	Serial.print("CCS811 library version: "); Serial.println(CCS811_VERSION);
+	setupI2c();
+	setupCcs811();
 }
 
 void loop() {
@@ -46,6 +28,7 @@ void loop() {
 		WiFi.reconnect();
 	}
 
+	//reconnect to mqtt server
 	if (!mqttClient.connected()) {
 		long now = millis();
 		if (now - lastMqttReconnectAttempt > 2000) {
@@ -56,27 +39,10 @@ void loop() {
 		}
 	} else {
 		mqttClient.loop();
-	}
 
-	if (five_seconds_delay()) {
-		uint16_t eco2, etvoc, errstat, raw;
-		ccs811.read(&eco2, &etvoc, &errstat, &raw);
-		Serial.print("CCS811: ");
-		if ( errstat == CCS811_ERRSTAT_OK ) {
-			Serial.print("eco2=");  Serial.print(eco2);  Serial.print(" ppm  ");
-			Serial.print("etvoc="); Serial.print(etvoc); Serial.print(" ppb  ");
-		} else if ( errstat == CCS811_ERRSTAT_OK_NODATA ) {
-			Serial.print("waiting for (new) data");
-		} else if ( errstat & CCS811_ERRSTAT_I2CFAIL ) {
-			Serial.print("I2C error");
-		} else {
-			Serial.print( "error: " );
-			Serial.print( ccs811.errstat_str(errstat) );
+		if (fiveSecondsDelay()) {
+			if(ccs811Read()) mqttClient.publish(CCS811_TOPIC, ccs811Reading);
 		}
-		Serial.println();
-
-		sprintf(mqttMsg, "{\"eco2\": \"%hu\", \"etvoc\": \"%hu\"}", eco2, etvoc);
-		mqttClient.publish(CCS811_TOPIC, mqttMsg);
 	}
 }
 
@@ -94,6 +60,29 @@ void setupMqtt() {
 	//mqttClient.setCallback(callback);
 }
 
+void setupI2c() {
+	Serial.print("Setup: I2C ");
+	Wire.begin();
+	Serial.println("OK");
+}
+
+void setupCcs811() {
+	//enable CCS811
+	bool ok;
+	Serial.print("Setup: CCS811 ");
+	ccs811.set_i2cdelay(50);
+	ok = ccs811.begin();
+	if (ok) Serial.println("OK"); else Serial.println("FAILED");
+
+	//start CCS811
+	Serial.print("Setup: CCS811 start ");
+	ok = ccs811.start(CCS811_MODE_1SEC);
+	if (ok) Serial.println("OK"); else Serial.println("FAILED");
+
+	//CCS811 version
+	Serial.print("CCS811 library version: "); Serial.println(CCS811_VERSION);
+}
+
 boolean mqttReconnect() {
 	Serial.println("Connecting to MQTT...");
 	if (mqttClient.connect("EspController1")) {
@@ -109,4 +98,26 @@ bool fiveSecondsDelay() {
 		timer = now;
 		return true;
 	} return false;
+}
+
+bool ccs811Read() {
+	uint16_t eco2, etvoc, errstat, raw;
+	ccs811.read(&eco2, &etvoc, &errstat, &raw);
+	Serial.print("CCS811: ");
+	if ( errstat == CCS811_ERRSTAT_OK ) {
+		Serial.print("eco2=");  Serial.print(eco2);  Serial.print(" ppm  ");
+		Serial.print("etvoc="); Serial.print(etvoc); Serial.print(" ppb  ");
+		Serial.println();
+		sprintf(ccs811Reading, "{\"eco2\": \"%hu\", \"etvoc\": \"%hu\"}", eco2, etvoc);
+		return true;
+	} else if ( errstat == CCS811_ERRSTAT_OK_NODATA ) {
+		Serial.print("waiting for (new) data");
+	} else if ( errstat & CCS811_ERRSTAT_I2CFAIL ) {
+		Serial.print("I2C error");
+	} else {
+		Serial.print( "error: " );
+		Serial.print( ccs811.errstat_str(errstat) );
+	}
+	Serial.println();
+	return false;
 }
