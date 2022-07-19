c6cf3e52bd73d2bfa9baf0eee71353fa801b2fe8
mamedesigor <mamedesigor@gmail.com>
2022-07-18 21:49:44 -0400

    fix physical quantities in website

--- website/static/js/request.js
+++ website/static/js/request.js
@@ -4,21 +4,18 @@ function update_fields() {
 			return response.json();
 		}).then(function (msg) {
 			document.getElementById("temp1").innerHTML = msg.temp1;
-			document.getElementById("amps1").innerHTML = msg.amps1;
+			document.getElementById("amps").innerHTML = msg.amps1;
 			document.getElementById("temp2").innerHTML = msg.temp2;
-			document.getElementById("amps2").innerHTML = msg.amps2;
 			document.getElementById("pressure").innerHTML = msg.pressure;
 			document.getElementById("humidity").innerHTML = msg.humidity;
 			document.getElementById("eco2").innerHTML = msg.eco2;
 			document.getElementById("tvoc").innerHTML = msg.tvoc;
-			document.getElementById("co").innerHTML = msg.co;
-			document.getElementById("no2").innerHTML = msg.no2;
-			document.getElementById("nh3").innerHTML = msg.nh3;
 			document.getElementById("presence1").innerHTML = msg.presence1;
 			document.getElementById("presence2").innerHTML = msg.presence2;
 			document.getElementById("pm25").innerHTML = msg.pm25;
 			document.getElementById("pm10").innerHTML = msg.pm10;
-			document.getElementById("door").innerHTML = msg.door;
+			document.getElementById("door1").innerHTML = msg.door1;
+			document.getElementById("door2").innerHTML = msg.door2;
 			document.getElementById("luxes").innerHTML = msg.luxes;
 			document.getElementById("instructions").innerHTML = msg.instructions;
 		});
