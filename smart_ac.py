1aceb39831032bca158720cf89cc3b0b4fb49552
mamedesigor <mamedesigor@gmail.com>
2022-08-08 17:43:28 -0400

    add log function

--- smart_ac.py
+++ smart_ac.py
@@ -12,8 +12,11 @@ buffer = config.buffer
 
 last_measurements = {}
 
+def log(msg):
+    print("["+ str(datetime.now().replace(microsecond=0)) + "] " + msg)
+
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    log("Connected with result code " + str(rc))
     client.subscribe(config.MQTT_TOPICS)
 
 def on_message(client, userdata, msg):
@@ -25,12 +28,12 @@ def on_message(client, userdata, msg):
 def send_post_request():
     url = 'https://www.arcondicionado.cf/request'
     if len(last_measurements) > 0:
-        print(last_measurements)
         try:
             requests.post(url, json = last_measurements, timeout = 5)
-            print("post request sent")
+            log("---------------- post request sent ----------------")
+            log(str(last_measurements))
         except:
-            print("An exception occurred when sending post request")
+            log("An exception occurred when sending post request")
         last_measurements.clear()
 
 def add_to_db():
@@ -48,9 +51,9 @@ def add_to_db():
                     last_measurements.update({"eco2": "eco2=" + eco2 + "ppm<br>" + timestamp})
                     tvoc = item.get("tvoc")
                     last_measurements.update({"tvoc": "tvoc=" + tvoc + "ppm<br>" + timestamp})
-                    print(eco2 + " " + tvoc + " " + timestamp)
                     cursor.execute("CREATE TABLE IF NOT EXISTS " + config.CCS811_TOPIC + " (eco2 real, tvoc real, timestamp text)")
                     cursor.execute("INSERT INTO " + config.CCS811_TOPIC + " VALUES(?,?,?)",(eco2, tvoc, timestamp))
+                    log(mqtt_topic + " added to db eco2: " + eco2 + " tvoc: " + tvoc)
 
             ### BMP280 ###
             if mqtt_topic == config.BMP280_TOPIC:
@@ -59,9 +62,9 @@ def add_to_db():
                     temp1 = item.get("temp1")
                     pressure = item.get("pressure")
                     timestamp = item.get("timestamp")
-                    print(temp1 + " " + pressure + " " + timestamp)
                     cursor.execute("CREATE TABLE IF NOT EXISTS " + config.BMP280_TOPIC + " (temp1 real, pressure real, timestamp text)")
                     cursor.execute("INSERT INTO " + config.BMP280_TOPIC + " VALUES(?,?,?)",(temp1, pressure, timestamp))
+                    log(mqtt_topic + " added to db temp1: " + temp1 + " pressure: " + pressure)
 
             ### SCT013 ###
             if mqtt_topic == config.SCT013_TOPIC:
@@ -69,9 +72,9 @@ def add_to_db():
                 for item in list:
                     amps = item.get("amps")
                     timestamp = item.get("timestamp")
-                    print(amps + " " + timestamp)
                     cursor.execute("CREATE TABLE IF NOT EXISTS " + config.SCT013_TOPIC + " (amps real, timestamp text)")
                     cursor.execute("INSERT INTO " + config.SCT013_TOPIC + " VALUES(?,?)",(amps, timestamp))
+                    log(mqtt_topic + " added to db amps: " + amps)
 
             buffer[mqtt_topic].clear()
     db.commit();
