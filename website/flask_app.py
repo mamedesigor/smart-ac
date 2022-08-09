a0c55030f80b34498d54df7d510749670a11764d
mamedesigor <mamedesigor@gmail.com>
2022-08-09 16:24:53 -0400

    update only new fields on post requests

--- website/flask_app.py
+++ website/flask_app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 import json
 
-data = {}
-
 app = Flask(__name__)
 
 @app.route('/request', methods=['GET', 'POST'])
 def requestfn():
-    global data
     if request.method == 'GET':
         with open('data.json') as f:
             data = json.load(f)
         return jsonify(data)
 
     if request.method == 'POST':
+        data = json.load(open('data.json'))
+        for key in request.json:
+            data.update({key: request.json[key]})
         with open('data.json', 'w') as f:
-            json.dump(request.json, f)
+            json.dump(data, f)
         return 'ok', 200
 
 @app.route('/')
